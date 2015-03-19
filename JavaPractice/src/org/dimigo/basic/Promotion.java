178f0f8216fa17530fc810cda433052198f15e98
skyclad1975 <skyclad1975@naver.com>
2015-03-19 09:27:07 +0900

    2625 - Promotion

---
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,15 @@
+package org.dimigo.basic;
+
+public class Promotion {
+	public static void main(String[] args){
+		long wage = 1700000;
+		int employ = 3;
+		int shop = 1500;
+		long sum = wage*employ*shop*12;
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : "+String.format("%,d", wage)+"원");
+		System.out.println("점포 내 직원 수 : "+employ+"명");
+		System.out.println("점포 수 : "+String.format("%,d", shop)+"개\n");
+		System.out.println("연간 인건비 : "+String.format("%,d", sum)+"원");
+	}
+}
