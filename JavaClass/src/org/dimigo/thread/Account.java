994875afb0ce0e25071aa6f49910d65940341a9f
Sanghyeon Jeon <skyclad0x7b7@gmail.com>
2015-11-16 09:19:19 +0900

    2625 webserver

--- JavaClass/src/org/dimigo/thread/Account.java
+++ JavaClass/src/org/dimigo/thread/Account.java
@@ -8,14 +8,36 @@ public class Account {
 	}
 	
 	public void deposit(int money){
-		this.balance += money;
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		synchronized(this){
+			this.balance += money;
+		}
 		System.out.println(money+"원 입금 => 현재 잔고  : "+this.balance);
 	}
 	
 	public void withdraw(int money){
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if(this.balance>=money){
-			this.balance -= money;
-			System.out.println(money+"원 출금 => 현재 잔고"+this.balance);
+			synchronized(this){
+				this.balance -= money;
+			}
+			System.out.println(money+"원 출금 => 현재 잔고 : "+this.balance);
 		}
 	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	
 }
--- JavaClass/src/org/dimigo/thread/AccountTest.java
+++ JavaClass/src/org/dimigo/thread/AccountTest.java
@@ -4,5 +4,38 @@ public class AccountTest {
 	public static void main(String[] args){
 		Account account = new Account(10000);
 		
+		Thread t1 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for(int i=0; i<5; i++){
+					account.deposit(2000);
+				}
+			}
+			
+		});
+		
+		Thread t2 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for(int i=0; i<3; i++){
+					account.withdraw(3000);
+				}
+			}
+			
+		});
+		
+		t1.start();
+		t2.start();
+		try{
+			t1.join();
+			t2.join();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("최종 잔액 : "+account.getBalance());
 	}
 }
